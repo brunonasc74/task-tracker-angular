105cad7933d523ca0ba0331948ae3eba85cef960
brunonasc74 <bruno.hpn74@gmail.com>
2022-11-25 19:17:01 -0300

    post method implemented

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -28,4 +28,10 @@ export class TasksComponent implements OnInit {
     task.reminder = !task.reminder;
     this.taskService.updateTask(task).subscribe();
   }
+
+  addTask(task: Task) {
+    this.taskService
+      .createTask(task)
+      .subscribe((task) => this.tasks.push(task));
+  }
 }
